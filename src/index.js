7770a19a70d48b907044e777abfc6635876719a3
Jayvon Rhines <jayvonrhines0@gmail.com>
2023-05-07 21:38:21 -0500

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.new_task.value)
+  } )
 });
+
+function handleToDo(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+ document.querySelector('#list').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
